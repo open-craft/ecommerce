mferenca/HMS-ecommerce

HMS-338 Allowed 0% discount coupon on frontend and backend

ecommerce/static/js/views/coupon_form_view.js
@@ -279,7 +279,7 @@ define([
                 var is_benefit_percentage = this.$('[name=benefit_type]:checked').val() === 'Percentage',
                     max_value = is_benefit_percentage ? '100' : '';
 
-                this.setLimitToElement(this.$('[name=benefit_value]'), max_value, 1);
+                this.setLimitToElement(this.$('[name=benefit_value]'), max_value, 0);
             },
 
             changeLimitForInvoiceDiscountValue: function () {
@@ -287,7 +287,7 @@ define([
                     '[name=invoice_discount_type]:checked').val() === 'Percentage',
                     max_value = is_invoice_discount_percentage ? '100' : '';
 
-                this.setLimitToElement(this.$('[name=invoice_discount_value]'), max_value, 1);
+                this.setLimitToElement(this.$('[name=invoice_discount_value]'), max_value, 0);
             },
 
             toggleDollarPercentIcon: function (val) {

open-craft/ecommerce

ecommerce/static/js/views/coupon_form_view.js
@@ -372,7 +372,7 @@ define([
                 var isBenefitPercentage = this.$('[name=benefit_type]:checked').val() === 'Percentage',
                     maxValue = isBenefitPercentage ? '100' : '';
 
-                this.setLimitToElement(this.$('[name=benefit_value]'), maxValue, 1);
+                this.setLimitToElement(this.$('[name=benefit_value]'), maxValue, 0);
             },
 
             changeLimitForInvoiceDiscountValue: function() {
@@ -380,7 +380,7 @@ define([
                     '[name=invoice_discount_type]:checked').val() === 'Percentage',
                     maxValue = isInvoiceDiscountPercentage ? '100' : '';
 
-                this.setLimitToElement(this.$('[name=invoice_discount_value]'), maxValue, 1);
+                this.setLimitToElement(this.$('[name=invoice_discount_value]'), maxValue, 0);
             },
 
             toggleDollarPercentIcon: function(val) {